6ac3f9f18f8cf179ee4d0e5dcbb3147070cc8f12
Archer <arseniy.tarapon@gmail.com>
2019-03-09 11:59:25 +0600

    Добавил модель Task и зарегистрировал ее в админке.

--- kanban_board/webapp/admin.py
+++ kanban_board/webapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from webapp.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'status']
+    search_fields = ['summary', 'id']
+
+
+admin.site.register(Task, TaskAdmin)
--- kanban_board/webapp/models.py
+++ kanban_board/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+TASK_STATUS_CHOICES = [
+    ('turn', 'Очередь'),
+    ('underway', 'В работе'),
+    ('done', 'Сделано'),
+]
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    due_date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=TASK_STATUS_CHOICES, default='turn')
+    time_planned = models.DecimalField(max_digits=4, decimal_places=1, null=True, blank=True)
+
+    def __str__(self):
+        return "%s (%s)" % (self.summary, self.status)
